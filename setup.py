6a27022fb3f77ee515d4aa3c4381c8bf0230879b
Michiel Brentjens <brentjens@astron.nl>
2010-10-05 15:29:38 +0200

    Beginning of v0.7 development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='rmsynthesis',
-      version='0.6',
+      version='0.7',
       description='Simple Faraday rotation measure synthesis tool',
       author='Michiel Brentjens',
       author_email='brentjens@astron.nl',
