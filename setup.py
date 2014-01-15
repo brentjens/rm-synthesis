e4dd362207609e4393819971ea540b20f47937ff
Michiel Brentjens <m.a.brentjens@gmail.com>
2014-01-15 11:35:30 +0100

    Changed version number to 1.0-rc4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='rmsynthesis',
-      version='1.0-rc3',
+      version='1.0-rc4',
       description='Simple Faraday rotation measure synthesis tool',
       author='Michiel Brentjens',
       author_email='brentjens@astron.nl',
