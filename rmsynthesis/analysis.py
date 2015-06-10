69c28aa26851a74adeca1c378e0e42eb5d32a860
Michiel Brentjens <brentjens@astron.nl>
2015-06-10 14:29:00 +0200

    Added averaging of PSF cubes to analysis module

--- rmsynthesis/analysis.py
+++ rmsynthesis/analysis.py
@@ -27,6 +27,7 @@ def correct_and_average_cubes(p_out_name, q_out_name, u_out_name,
         ignore_frames = [[]]*len(qu_input_names)
 
     p_out_hdr, q_out_hdr, u_out_hdr = output_pqu_headers(q_header)
+    logging.info('P header:\n%r', p_out_hdr)
     p_out, q_out, u_out = [fits.streaming_output_hdu(fits_name,
                                                      header,
                                                      force_overwrite)
@@ -41,17 +42,60 @@ def correct_and_average_cubes(p_out_name, q_out_name, u_out_name,
         wl2 = wavelength_squared_m2_from_freq_hz(freq)
         q_frames = array(freq_qu_list[1::2], dtype=float32)
         u_frames = array(freq_qu_list[2::2], dtype=float32)
-        mask = array([channel in ignore for ignore in ignore_frames])
+        mask = 1.0 - array([channel in ignore for ignore in ignore_frames])
         phasors = exp(-2.j*array(rm_to_remove)*wl2)*mask
         corr_p_frames = (q_frames +1j*u_frames)*phasors[:, newaxis, newaxis]
-        p_final = corr_p_frames.sum(axis=0)/mask.sum()
+        if mask.sum() == 0.0:
+            p_final = corr_p_frames.sum(axis=0)*0.0
+        else:
+            p_final = corr_p_frames.sum(axis=0)/float(mask.sum())
         weights.append(mask.sum())
-        p_out.write(abs(p_final))
-        q_out.write(p_final.real)
-        u_out.write(p_final.imag)
+        p_out.write(array(abs(p_final), dtype=float32))
+        q_out.write(array(p_final.real, dtype=float32))
+        u_out.write(array(p_final.imag, dtype=float32))
     p_out.close()
     q_out.close()
     u_out.close()
     with open(weights_name, 'w') as w_out:
         w_out.write('\n'.join([str(w) for w in weights]))
     logging.info('Done correcting')
+
+
+
+
+
+def average_psf_cubes(psf_out_name,
+                      weights_name,
+                      psf_input_names,
+                      force_overwrite=False,
+                      ignore_frames=None):
+    r'''
+    TBD
+    '''
+    logging.info('correct_and_average_cubes()')
+    psf_header = fits.get_header(psf_input_names[0])
+    psf_iterators = [fits.image_frames(name) for name in psf_input_names]
+
+    if ignore_frames is None:
+        ignore_frames = [[]]*len(qu_input_names)
+
+    psf_out_hdr = psf_header
+    logging.info('PSF header:\n%r', psf_out_hdr)
+    psf_out = fits.streaming_output_hdu(psf_out_name, psf_header, force_overwrite)
+
+    weights = []
+    for channel, frames in enumerate(izip(*psf_iterators)):
+        logging.info('Channel %r', channel)
+        psf_frames = array(frames, dtype=float32)
+        mask = 1.0 - array([channel in ignore for ignore in ignore_frames])
+        masked_psf_frames = psf_frames*mask[:, newaxis, newaxis]
+        if mask.sum() == 0.0:
+            psf_final = psf_frames[0,:,:]*0.0
+        else:
+            psf_final = psf_frames.sum(axis=0)/float(mask.sum())
+        weights.append(mask.sum())
+        psf_out.write(array(psf_final, dtype=float32))
+    psf_out.close()
+    with open(weights_name, 'w') as w_out:
+        w_out.write('\n'.join([str(w) for w in weights]))
+    logging.info('Done averaging')
